cecb1f961a05a4e7b9e7ef6ba5e06e821fcfc0f0
davidelunetta <davidelunetta.27@gmail.com>
2023-12-20 16:16:51 +0100

    modified route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[PageController::class,'index']);
+Route::get('books',[BookController::class,'index']);
